7e6d41d11efa669555f79a67fdbc497aecb18411
Pixelpip <athulkamathg5@gmail.com>
2024-08-09 23:26:42 +0530

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
