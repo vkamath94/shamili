82d52c9957028bdffda0e7f3689ed236e10f950b
Pixelpip <athulkamathg5@gmail.com>
2024-08-13 18:15:12 +0530

    msg

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import Image from 'next/image';
+import Hero1 from '/public/College.jpeg'; 
+import Hero2 from '/public/Hero2.jpeg'
 
 const Hero = () => {
   return (
@@ -18,17 +21,19 @@ const Hero = () => {
               className="h-full w-full"
             >
               <SwiperSlide>
-                <img
+                <Image
                   alt="Slide 1"
-                  src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  className="h-full w-full object-cover"
+                  src={Hero1}
+                 layout="fill" 
+                  objectFit="cover"
                 />
               </SwiperSlide>
               <SwiperSlide>
-                <img
+                <Image
                   alt="Slide 2"
-                  src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  className="h-full w-full object-cover"
+                  src={Hero2}
+                  layout="fill" 
+                  objectFit="fill"
                 />
               </SwiperSlide>
               {/* Add more SwiperSlides here as needed */}
@@ -36,20 +41,15 @@ const Hero = () => {
           </div>
 
           <div className="lg:py-24">
-            <h2 className="text-3xl font-bold sm:text-4xl">Grow your audience</h2>
+            <h2 className="text-3xl font-bold sm:text-4xl">Shmaili Pre University College</h2>
 
-            <p className="mt-4 text-gray-600">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-              eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
-              quidem quam repellat.
+            <p className="mt-4 text-gray-700">
+              Experience a nurturing environment where academic excellence meets holistic development. 
+              Our institution provides a dynamic curriculum and dedicated faculty, ensuring every 
+              student achieves their highest potential
             </p>
 
-            <a
-              href="#"
-              className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-            >
-              Get Started Today
-            </a>
+           
           </div>
         </div>
       </div>
